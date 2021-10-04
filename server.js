05f4858400f68761d755ef5f52cd6329557cb14b
Gökberk Otlu <gokberkotlu@gmail.com>
2021-10-04 14:06:48 +0300

    server.js middlewares update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+    static: './build'
+});
 const port = process.env.PORT || 4000;
 
 server.use(middlewares);

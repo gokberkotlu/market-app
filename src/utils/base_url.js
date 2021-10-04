1165e0e886e4adbc8efc10bc7003d3d039ff5e70
Gökberk Otlu <gokberkotlu@gmail.com>
2021-10-04 13:32:56 +0300

    base_url change

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
 // export const base_url = "http://localhost:4000/";
-export const base_url = "https://market-app-study.herokuapp.com/api/";
+export const base_url = "https://market-app-study.herokuapp.com/";

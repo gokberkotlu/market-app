6543420e557addd422a44a59108916173ce25b1d
Gökberk Otlu <gokberkotlu@gmail.com>
2021-10-04 06:59:45 +0300

    index.html: title & content changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./components/navbar/Navbar";
 import AppBody from "./components/app-body/AppBody";
-import Footer from "./components/footer/Footer";
 
 function App() {
   return (
